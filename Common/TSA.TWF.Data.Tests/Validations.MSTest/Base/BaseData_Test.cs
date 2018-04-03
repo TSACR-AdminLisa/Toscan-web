5d9cdec02440f7af45aaf93c779ba24618caf362
lvarela <limavasol@outlook.com>
2018-04-03 00:46:46 -0600

    proyecto de pruebas primera subida

---
+++ Common/TSA.TWF.Data.Tests/Validations.MSTest/Base/BaseData_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using Common.Validations.Base;
+
+namespace Validations.MSTest.Base
+{
+    [TestClass]
+    public class BaseData_Test
+    {
+
+
+
+    }
+}
